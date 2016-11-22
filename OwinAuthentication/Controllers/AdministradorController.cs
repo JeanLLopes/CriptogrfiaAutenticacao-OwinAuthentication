79d27545164f13480538ec172aedb278a4a0bcfe
JeanLLopes <jeanllopes@outlook.com>
2016-11-22 21:27:28 -0200

    Initial commit

---
+++ OwinAuthentication/Controllers/AdministradorController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OwinAuthentication.Controllers
+{
+    //Aqui só devera entrar administrador!!!
+    public class AdministradorController : Controller
+    {
+        // GET: Administrador
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ OwinAuthentication/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿
+using OwinAuthentication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+//Para se autenticar via FormsAuthentication
+//Temos que usar o seguinte Namespace:
+using System.Web.Security;
+
+namespace OwinAuthentication.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ValidateAntiForgeryToken] //Validando a origem.
+        [HttpPost]
+        public ActionResult Index(LoginModel login, string returnUrl)
+        {
+            return View();
+        }
+
+        public ActionResult LogOff() {
+            
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
